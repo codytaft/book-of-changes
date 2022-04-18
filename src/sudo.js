a3c02deb275524794d4590bb67b1241bdf482c80
Cody Taft <cody.taft@gmail.com>
2022-04-17 17:19:41 -0700

    sudo code

---
+++ src/sudo.js
@@ -0,0 +1,31 @@
+// stalks = 50 - 1
+
+const getLine = (stalks) => {
+    let right = // get random 4:stalks - 4
+    right -= 1
+    const pinkyFinger = 1
+    
+    let left = stalks - random;
+    
+    while (left >= 4) {
+        left -= 4
+    }
+    
+    const ringFinger = left
+    
+    while (right >= 4) {
+        right -= 4
+    }
+    
+    const middleFinger = right
+    const calculation = pinkyFinger + ringFinger + middleFinger
+    
+    return convertLine(calculation)
+}
+
+const convertLine = (lineCalculation) => {
+    switch (lineCalculation) {
+        case 9:
+        
+    }
+}
